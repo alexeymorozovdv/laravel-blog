6c116aa069d49cedecb8eb3126cf0f64ce4f1a9c
Alexey Morozov <alexeymorozov.dev@gmail.com>
2021-11-16 08:15:29 +0300

    Created models, factories and migrations for blog's posts, categories and comments

---
+++ database/migrations/2021_11_16_051300_create_blog_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_comments', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained('blog_posts');
+
+            $table->text('body');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_comments');
+    }
+}
